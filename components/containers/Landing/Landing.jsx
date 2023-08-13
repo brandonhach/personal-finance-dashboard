232d663ef80ffd3b5788d283bcb20f43966b8d43
brandonhach <specialcambo@gmail.com>
2023-08-13 00:17:54 -0400

    add landing

--- components/containers/Landing/Landing.jsx
+++ components/containers/Landing/Landing.jsx
@@ -3,7 +3,17 @@ import React from "react";
 const Landing = () => {
 	return (
 		<>
-			<div className="container flex flex-col items-center justify-center bg-green-200">
+			<div className="container flex flex-col items-center justify-center bg-black w-auto h-screen">
+				<div className="container flex flex-col items-center text-white text-left text-5xl font-semibold w-1/2 gap-5">
+					<h3 className="container ">
+						Understand your customers by viewing the metric of your company.
+					</h3>
+					<h3 className="container">Maintain and track your expenses and profits.</h3>
+					<h3 className="container">Predict your performance through our dashboard.</h3>
+				</div>
+				<div></div>
+			</div>
+			<div className="container flex flex-col items-center justify-center h-screen ">
 				<div></div>
 				<div></div>
 			</div>
--- components/layout/Header/Header.jsx
+++ components/layout/Header/Header.jsx
@@ -1,23 +1,27 @@
 import React from "react";
-import Link from "next/link";
 import { FaArrowDown } from "react-icons/fa";
 
 const Header = () => {
 	return (
 		<>
-			<header className="relative flex flex-col items-center justify-center h-screen mb-12 overflow-hidden">
+			<header className="relative flex flex-col items-center justify-center h-screen overflow-hidden">
 				<video
 					className="absolute z-10 w-auto min-w-full min-h-full object-fill"
-					autoPlay="true"
+					autoPlay={true}
 					loop
 					muted>
 					<source src="Video/black abstract.mp4" />
 				</video>
 				<div className="container flex flex-col items-center justify-center">
-					<h1 className="z-30 p-5 text-white bg-opacity-50 rounded-xl shadow-md text-7xl">
+					<h1
+						className="z-30 p-5 text-white bg-opacity-50 rounded-xl shadow-md text-7xl
+					">
 						HachView
 					</h1>
-					<h2 className="z-30 p-5 text-2xl text-white bg-opacity-50 rounded-xl shadow-md">
+					<h2
+						className="z-30 p-5 text-2xl text-white bg-opacity-50 rounded-xl shadow-md text-center
+						
+					">
 						Use machine learning to predict your future financial forecast.
 					</h2>
 				</div>
